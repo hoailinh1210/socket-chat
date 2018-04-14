7af8de1361718dd53eabde23e7b8c1faab60baee
hoailinh1210 <builhoailinh@dtu.edu.vn>
2018-04-14 23:09:24 +0700

    Fix every request

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ io.on('connection',function(socket){
     socket.on('chat message',msg => {
         console.log(msg);
     })
+    socket.on('disconnect',function(){
+        console.log('user disconnected');
+    })
 })
 
 http.listen(8032,'localhost',function(){
