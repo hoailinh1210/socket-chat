175ef7331baeb83f8bd7c86082ca8d436abdd944
hoailinh1210 <builhoailinh@dtu.edu.vn>
2018-04-16 20:52:01 +0700

    update count online, broadcast support name

--- server.js
+++ server.js
@@ -1,18 +1,37 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
+
 
 app.get('/',function(req,res,next){
     res.sendfile(__dirname + '/index.html');
 });
+
+var count = 0;
+var users = {};
+
 io.on('connection',function(socket){
     console.log('a user connected');
+    var id = socket.id;
+    count++;
+    io.emit('online',{count});
+    socket.on('user name',na => {
+        users[socket.id] = na;
+        socket.name = na;
+        io.emit('online',{users:users,id: socket.id});
+    })
+
     socket.on('chat message',msg => {
-        console.log(msg);
-        io.emit('chat message','hi');
+        io.emit('chat message',{msg,name: socket.name});
     })
     socket.on('disconnect',function(){
         console.log('user disconnected');
+        count--;
+        io.emit('online',{count});
     })
 })
 
